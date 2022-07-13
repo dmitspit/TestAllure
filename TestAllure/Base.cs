2e742fd91565e4e7234e91d290801cc2cb8616c9
Dmytro <dmitspit@gmail.com>
2022-07-13 14:36:35 +0300

    Add project files.

---
+++ TestAllure/Base.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestAllure
+{
+    public class Base
+    {
+        private ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+
+      
+        protected IWebDriver Driver => driver.Value;
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+           
+
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver.Value = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+ 
+
+            Driver.Quit();
+           
+        }
+
+    }
+}
---
+++ TestAllure/UnitTest1.cs
@@ -0,0 +1,49 @@
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System.Threading;
+
+namespace TestAllure
+{
+    [TestFixture]
+    [AllureNUnit]
+    [Category("API validation Test")]
+    [Parallelizable(ParallelScope.All)]
+    public class Tests : Base
+    {
+        [Test]
+        [Category("C7552714")]
+        [Category("Regrsion")]
+        [Description("Verify Fee and Tax Transmission and transformation to PMP If FeeSettings = 1 and Affiliate Tax is not specified")]
+        [Author("Dmytro")]
+        public void _01_Test()
+        {
+            Driver.Navigate().GoToUrl("https://www.google.com");
+            Thread.Sleep(2000);
+            Assert.True(Driver.Title.Contains("Google"));
+        }
+
+        [Test]
+        [Category("C7557744")]
+        [Category("Regrsion")]
+        [Description("Verify Fee and Tax Transmission and transformation to PMP If FeeSettings = 1 and Affiliate Tax is not specified")]
+        [Author("Dmytro")]
+        public void _02_Test()
+        {
+            Driver.Navigate().GoToUrl("https://www.google.com");
+            Thread.Sleep(2000);
+            Assert.True(Driver.Title.Contains("Google"));
+        }
+
+        [Test]
+        [Category("C7552714"), Category("C7558432"), Category("C7557743"), Category("C7557744")]
+        [Category("Regrsion")]
+        [Description("Verify Fee and Tax Transmission and transformation to PMP If FeeSettings = 1 and Affiliate Tax is not specified")]
+        [Author("Dmytro")]
+        public void _03_Test_name()
+        {
+            Driver.Navigate().GoToUrl("https://www.google.com");
+            Thread.Sleep(3000);
+            Assert.AreEqual("AXqw", Driver.Title, "Title is not as expected");
+        }
+    }
+}
